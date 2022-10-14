373da23185b478c22cbf92b19f1383aef931a47d
devandewa <devandewananta@gmail.com>
2022-10-14 11:25:23 +0700

    relasi role model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function role(){
-        return $this->belongsTo(ComCode::class, 'role_st');
-    }
+    // public function role(){
+    //     return $this->belongsTo(ComCode::class, 'role_st');
+    // }
 }
