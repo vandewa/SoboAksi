6e171f6d0cf507bfe407330c09b8b349c6d35e87
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2022-10-31 10:02:05 +0700

    penambahan property donasi

---
+++ database/migrations/2022_10_31_014126_create_donasi_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donasi_payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('aksi_id');
+            $table->unsignedBigInteger('aksi_penerima_id');
+            $table->double("payment");
+            $table->dateTime("request_payment");
+            $table->dateTime("payment_date")->nullable();
+            $table->string("payment_status");
+            $table->string("payment_type");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donasi_payments');
+    }
+};
---
+++ database/migrations/2022_10_31_015545_create_ewallets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ewallets', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->boolean('use_phone')->default(false);
+            $table->boolean('use_cashtag')->default(false);
+            $table->float('fee');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ewallets');
+    }
+};
