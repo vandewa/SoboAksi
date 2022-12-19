f625c28fd0c57c2ccfd77ac83eaf961bfaae9620
devandewa <devandewananta@gmail.com>
2022-12-19 14:32:53 +0700

    tambah template front

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin/dashboard');
+        // return view('admin/dashboard');
+
+        return view('front.beranda');
     }
 }
