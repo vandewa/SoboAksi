ea326fb3638f366b1030ac49a2f2b9a4d0980613
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2022-12-20 16:26:58 +0700

    tambah model tapi error

--- app/Http/Livewire/Home/FormRegister.php
+++ app/Http/Livewire/Home/FormRegister.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class FormRegister extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public $tgl_lahir;
+    public $nomor_hp;
+    public $password_confirmation;
+
     public function render()
     {
         return view('livewire.home.form-register');
--- resources/views/livewire/home/form-register.blade.php
+++ resources/views/livewire/home/form-register.blade.php
@@ -5,39 +5,42 @@
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 donate-form">
             <div class="form-inner">
-                <h3>Donar Information</h3>
+                <h3>Pendaftaran</h3>
                 <div class="row clearfix">
                     <div class="col-lg-6 col-md-6 col-sm-12 column">
                         <div class="form-group">
-                            <label>Your Name <span>*</span></label>
-                            <input type="text" name="name" placeholder="example name" required="">
+                            <label>Nama <span>*</span></label>
+                            <input type="text" name="name" placeholder="example name" required="" wire:model="name">
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 column">
                         <div class="form-group">
-                            <label>Email Address <span>*</span></label>
-                            <input type="email" name="email" required="">
+                            <label>Alamat Email <span>*</span></label>
+                            <input type="email" name="email" required=""  wire:model="email">
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 column">
                         <div class="form-group">
-                            <label>Phone</label>
-                            <input type="text" name="phone" required="">
+                            <label>Nomor HP <span>*</span></label>
+                            <input type="text" name="hp" required=""  wire:model="nomor_hp">
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 column">
                         <div class="form-group">
-                            <label>Address</label>
-                            <input type="text" name="address" required="">
+                            <label>Tanggal Lahir <span>*</span></label>
+                            <input type="date" name="tgl_lahir" required=""  wire:model="tgl_lahir">
                         </div>
                     </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12 column">
+                    <div class="col-lg-6 col-md-6 col-sm-12 column">
                         <div class="form-group">
-                            <label class="custom-control material-checkbox">
-                                <input type="checkbox" class="material-control-input">
-                                <span class="material-control-indicator"></span>
-                                <span class="description">I would like to donate automatically repeat each month</span>
-                            </label>
+                            <label>Password</label>
+                            <input type="password" name="phone" required=""  wire:model="password">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 column">
+                        <div class="form-group">
+                            <label>Konfirmasi Password</label>
+                            <input type="password" name="phone" required="" wire:model="password_confirmation">
                         </div>
                     </div>
                     <div class="col-lg-12 col-md-12 col-sm-12 column">
