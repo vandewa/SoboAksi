e48db13bce972f75d7ed5e6400f044b4122b8901
devandewa <devandewananta@gmail.com>
2022-12-26 12:48:00 +0700

    a

--- resources/views/livewire/global/card-donasi.blade.php
+++ resources/views/livewire/global/card-donasi.blade.php
@@ -1,16 +1,15 @@
-<div class="row clearfix">
-    @foreach ($aksi as $aksi)
+
         <div class="col-lg-4 col-md-6 col-sm-12 cause-block">
             <div class="cause-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
                 <div class="inner-box">
                     <div class="image-box">
                         <figure class="image"><a href="causes-details.html"><img src="{{ asset('trusthand/assets/images/resource/cause-1.jpg')}}" alt=""></a></figure>
-                        <div class="category"><a href="causes-details.html">{{$aksi->kategorinya->nama_kategori??"-"}}</a></div>
+                        <div class="category"><a href="causes-details.html">{{$kategorinya->nama_kategori}}</a></div>
                     </div>
                     <div class="lower-content">
                         <div class="text">
-                            <h3><a href="causes-details.html">{{$aksi->judul??"-"}}</a></h3>
-                            <p>{{$aksi->deskripsi??"-"}}</p>
+                            <h3><a href="causes-details.html">{{$judul}}</a></h3>
+                            <p>{{$deskripsi}}</p>
                         </div>
                         <div class="progress-box">
                             <div class="bar">
@@ -20,10 +19,16 @@
                                 <h6><span>$5,020</span> Raised</h6>
                                 <h6><span>$8,000</span> Target</h6>
                             </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6">
+                                    <button class="btn btn-primary btn-block"> <i class="icon-16"></i> Komen</button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn-warning btn-block"> <i class="fa fa-heart"></i> Suka</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    @endforeach
-</div>
