6dacde83f4c1730e61fb12a3afa3a1fa577fde48
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2023-02-23 08:37:14 +0700

    penambahan retry

--- app/Http/Livewire/Article.php
+++ app/Http/Livewire/Article.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
+use Exception;
+use Illuminate\Http\Client\PendingRequest;
 
 class Article extends Component
 {
@@ -14,9 +16,18 @@ class Article extends Component
 
     public function mount()
     {
-        $response = Http::withoutVerifying()->get('https://diskominfo.wonosobokab.go.id/api/news');
-        $response = $response->collect();
-        $this->article =   array_slice($response['data']['data'], 0, 4);
+        $response = Http::withoutVerifying()->retry(5, 1000, function (Exception $exception, PendingRequest $request) {
+            return false;
+        })->get('https://diskominfo.wonosobokab.go.id/api/news');
+
+
+        if($response->clientError()){
+
+        } else {
+            $response = $response->collect();
+
+            $this->article =   array_slice($response['data']['data'], 0, 4);
+        }
     }
 
 
